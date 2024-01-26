33c63f1fd9d1c2ca04df99c248e2c87ceea0a672
Lucas-Maldo <ldmaldonado@miuandes.cl>
2024-01-26 15:30:16 -0300

    Optimized movment conditions and modified board display

--- Damas.py
+++ Damas.py
@@ -4,8 +4,9 @@ Created on Tue Apr 30 20:05:12 2019
 
 @author: lucas
 """
+from test import movida_valida_v2
 
-#combine pos1 and pos2 into one list 
+#Keep testing for player 2
 Tablero = [["X","_","_","_","_","_","_","_"],
            ["_","X","_","_","_","_","_","_"],
            ["_","_","_","_","_","_","_","_"],
@@ -14,12 +15,32 @@ Tablero = [["X","_","_","_","_","_","_","_"],
            ["_","X","_","O","_","X","_","_"],
            ["_","_","_","_","_","_","_","_"],
            ["_","_","_","_","_","_","_","_"]]
+Tablero = [["_","_","_","_","_","_","_","_"],
+           ["_","O","_","_","_","_","_","_"],
+           ["_","_","O","_","X","_","O","_"],
+           ["_","_","_","_","_","_","_","_"],
+           ["_","_","_","_","X","_","O","_"],
+           ["_","_","_","_","_","_","_","_"],
+           ["_","_","_","_","_","_","O","_"],
+           ["_","_","_","_","_","_","_","O"]]
+player1 = "X"
+player2 = "O"
+
 #Damas
 def imprimir_tablero(matriz):
+    print("  ", end = " ")
     for i in range(len(matriz)):
+        print(i, end = " ")
+    print()
+    for i in range(len(matriz)):
+        print(i, end = " ")
         for j in range(len(matriz)):
             print('|' + matriz[i][j], end = "")
-        print("|")
+        print("|", i)
+    print("  ", end = " ")
+    for i in range(len(matriz)):
+        print(i, end = " ")
+    print()
 
 def crear_tablero():
     matriz=[]
@@ -33,129 +54,121 @@ def crear_tablero():
 
 def armar_tablero(matriz):
     num1 = 12
-    num2 = 12
     for i in range(len(matriz)):
         for j in range(1 if i%2==0 else 0,len(matriz),2):
             if(num1 > 0):
-                matriz[i][j] = "X"
-                matriz[-i-1][j] = "O"
+                matriz[i][j] = player1
+                matriz[-i-1][j] = player2
                 num1-=1
     return matriz
-#CHECK FOR JUMPS for second player (maybe can be changed from before to be unnecesary)
+
 #CHECK FOR KINGS, maybe just add an or case for if piece == QQ or another letter
-def movida_valida(player, pos1, pos2, matriz):
-    len_pos2 = len(pos2)
-    #Check if inside board
-    if(any((a[0]<0 or a[0]>7 or a[1]<0 or a[1]>7) for a in pos2)):
+def movida_valida_v2(player_sym, enemy_sym, pos, matriz):
+    if(player_sym == "O"):
+        pos = pos[::-1]
+    saltos = len(pos) - 1
+
+    print("Checking if inside board") #perfect
+    if(any((a[0]<0 or a[0]>7 or a[1]<0 or a[1]>7) for a in pos)):
            return "coordinate outside of map"
-    if(player == 1):
-        #Check if forward
-        if(pos1[0] >= pos2[0][0]):
-            return "should be forward1"
-        for i in range(len(pos2[0:-1])):
-            if(pos2[i][0] >= pos2[i+1][0]):
-                return "should be forward2"
-        #Check diagonal
-        if(pos2[0][0] - pos1[0] != abs(pos2[0][1] - pos1[1])):
-            return "should be diagonal1"
-        for i in range(len(pos2[0:-1])):
-            if(pos2[i+1][0] - pos2[i][0] != abs(pos2[i+1][1] - pos2[i][1])):
-                return "should be diagonal2"
-    if(player == 2):
-        #Check if forward
-        if(pos1[0] <= pos2[0][0]):
-            return "should be forward1"
-        for i in range(len(pos2[0:-1])):
-            if(pos2[i][0] <= pos2[i+1][0]):
-                return "should be forward2"
-        #Check if diagonal
-        if(pos1[0] - pos2[0][0] != abs(pos1[1] - pos2[0][1])):
-            return "should be diagonal1"
-        for i in range(len(pos2[0:-1])):
-            if(pos2[i][0] - pos2[i+1][0] != abs(pos2[i][1] - pos2[i+1][1])):
-                return "should be diagonal2"
-    #Check jump
+
+    print("Checking if forward")#Should be ok but check for player 2
+    for i in range(saltos):
+        if(pos[i][0] >= pos[i+1][0]):
+            return "should be forward"
+        
+    print("Checking if diagonal")#Should be ok but check for player 2
+    for i in range(saltos):
+        if(pos[i+1][0] - pos[i][0] != abs(pos[i+1][1] - pos[i][1])):
+            return "should be diagonal"
+
+    print("Checking for jump") #Have to check for multiple cases       
     jump = False
-    #Esto podria estar malo para jugador 2
-    if(matriz[pos1[0]+1][pos1[1]+(pos2[0][1] - pos1[1])//2] != "_"):
-        if(len(pos2)>1):
-            print(len(pos2))
-            for i in range(len(pos2[0:-1])):
-                if(matriz[pos2[i][0]+1][pos2[i][1]+(pos2[i+1][1] - pos2[i][1])//2] != "_"):
-                    print("multiple jumps")
-                    jump = True
-        else:
-            # print("single jump")
+    for i in range(saltos):
+        if(matriz[pos[i][0] + 1][pos[i][1] + (pos[i+1][1] - pos[i][1])//2] != "_"):
+            print("valid jump")
             jump = True
+
     if(jump):
-        print("Entro")
-        if(player == 1):
-            if(pos2[0][0] - pos1[0] != 2):
-                return "invalid jump"
-            for i in range(len(pos2[0:-1])):
-                if(pos2[i+1][0] - pos2[i][0] != 2):
-                    return "invalid jump to"+str(pos2[i+1])
-            matriz[pos1[0]][pos1[1]] = "_"
-            if(matriz[pos1[0]+1][pos1[1]+(pos2[0][1] - pos1[1])//2] == "O"):
-                matriz[pos1[0]+1][pos1[1]+(pos2[0][1] - pos1[1])//2] = "_"
-            for i in range(len(pos2[0:-1])):
-                if(matriz[pos2[i][0] + 1][pos2[i][1] + (pos2[i+1][1] - pos2[i][1])//2] == "O"):
-                   matriz[pos2[i][0] + 1][pos2[i][1] + (pos2[i+1][1] - pos2[i][1])//2] = "_"
-            matriz[pos2[-1][0]][pos2[-1][1]] = "X"
-            # imprimir_tablero(Tablero)
-        if(player == 2):
-            if(pos1[0] - pos2[0][0] != 2):
-                return "invalid jump"
-            for i in range(len(pos2[0:-1])):
-                if(pos2[i][0] - pos2[i+1][0] != 2):
-                    return "invalid jump to"+str(pos2[i+1])
-            matriz[pos1[0]][pos1[1]] = "_"
-            if(matriz[pos1[0] - 1][pos1[1]- (pos1[1] - pos2[0][1])//2] == "X"):
-                matriz[pos1[0] - 1][pos1[1]- (pos1[1] - pos2[0][1])//2] = "_"
-            for i in range(len(pos2[0:-1])):
-                if(matriz[pos2[i][0] - 1][pos2[i][1] - (pos2[i][1] - pos2[i+1][1])//2] == "X"):
-                   matriz[pos2[i][0] - 1][pos2[i][1] - (pos2[i][1] - pos2[i+1][1])//2] = "_"
-            matriz[pos2[-1][0]][pos2[-1][1]] = "O"
-            imprimir_tablero(Tablero)
+        for i in range(saltos):
+            if(pos[i+1][0] - pos[i][0] != 2):
+                if(player_sym == "O"):
+                    return "invalid jump to "+ str(pos[i])
+                return "invalid jump to "+ str(pos[i+1])
+        #Applying jump
+        if(player_sym == "O"):
+            matriz[pos[-1][0]][pos[-1][1]] = "_"
+        else:
+            matriz[pos[0][0]][pos[0][1]] = "_"
+        for i in range(saltos):
+            if(matriz[pos[i][0] + 1][pos[i][1] + (pos[i+1][1] - pos[i][1])//2] == enemy_sym):
+                matriz[pos[i][0] + 1][pos[i][1] + (pos[i+1][1] - pos[i][1])//2] = "_"
+        if(player_sym == "O"):
+            matriz[pos[0][0]][pos[0][1]] = player_sym
+        else:
+            matriz[pos[-1][0]][pos[-1][1]] = player_sym
 
     else:
-        if(player == 1):
-            if(pos2[0][0] - pos1[0] + abs(pos2[0][1] - pos1[1]) > 2):
-                return "should be no more than one space"
-        
+        if(pos[1][0] - pos[0][0] > 1):
+            return "should be no more than one space"        
 
 
-# Tablero = crear_tablero()
-# Tablero = armar_tablero(Tablero)
+Tablero = crear_tablero()
+Tablero = armar_tablero(Tablero)
 imprimir_tablero(Tablero)
 
 while True:
+    #Player 1 turn
     while True:
-        posicion1 = input("Select a piece by its coordinates: ").split(",")
-        posicion1 = tuple(int(item) for item in posicion1)
+        posicion1 = input("Player " + player1 + " select a piece by its coordinates: ").split(",")
+        posicion1 = list(int(item) for item in posicion1)
         print(posicion1)
-        pos = Tablero[posicion1[0]][posicion1[1]]
-        if(pos != "X"):
+        if(Tablero[posicion1[0]][posicion1[1]] != player1):
             print("Not a valid piece")
             continue
-    # while True:
         posicion2 = input("Select a place to move to: ").split(" ")
-        posicion2 = list(tuple(int(item) for item in coord.split(",")) for coord in posicion2)
-        print(posicion2)
+        posicion2 = list(list(int(item) for item in coord.split(",")) for coord in posicion2)
+        #Check if valid space
         pos = []
         for i in posicion2:
             pos.append(Tablero[i[0]][i[1]])
-        #Check if valid space
         if(any(coordinate != "_" for coordinate in pos)):
             print("Requires empty square")
             continue
-        error = movida_valida(1, posicion1, posicion2, Tablero)
+        error = movida_valida_v2(player1, player2, [posicion1] + posicion2, Tablero)
         if(error):
             #Contador con todas las movidas posibles? y si eso igual a cantidad de errores entonces gano el otro?
             print("Not a valid movement,", error)
             continue
         else:
             print("Valid move")
+            imprimir_tablero(Tablero)
+            break
+    #Player 2 turn
+    while True:
+        posicion1 = input("Player " + player2 + " select a piece by its coordinates: ").split(",")
+        posicion1 = list(int(item) for item in posicion1)
+        print(posicion1)
+        if(Tablero[posicion1[0]][posicion1[1]] != player2):
+            print("Not a valid piece")
+            continue
+        posicion2 = input("Select a place to move to: ").split(" ")
+        posicion2 = list(list(int(item) for item in coord.split(",")) for coord in posicion2)
+        #Check if valid space
+        pos = []
+        for i in posicion2:
+            pos.append(Tablero[i[0]][i[1]])
+        if(any(coordinate != "_" for coordinate in pos)):
+            print("Requires empty square")
+            continue
+        error = movida_valida_v2(player2, player1, [posicion1] + posicion2, Tablero)
+        if(error):
+            #Contador con todas las movidas posibles? y si eso igual a cantidad de errores entonces gano el otro?
+            print("Not a valid movement,", error)
+            continue
+        else:
+            print("Valid move")
+            imprimir_tablero(Tablero)
             break
     break
 #         fila = posicion//3
